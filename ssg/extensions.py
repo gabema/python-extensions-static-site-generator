0fe9496f9c3276f9b294c7c5f47aed795bd28d9a
Gabe Martin <gabema@gmail.com>
2022-09-17 09:55:24 -0700

    moving functions to correct module

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -3,27 +3,6 @@ import sys
 import importlib
 from pathlib import Path
 
-
-_callbacks = {}
-
-def register(hook, order = 0):
-    def register_callback(func):
-        _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
-        return func
-
-    return register_callback
-
-def event(hook, *args):
-    for order in sorted(_callbacks.get(hook, {})):
-        for func in _callbacks[hook][order]:
-            func(*args)
-
-def filter(hook, value, *args) :
-    for order in sorted(_callbacks.get(hook, {})) :
-        for func in _callbacks[hook][order] :
-            value = func(value, *args)
-    return value
-
 def load_module(directory, name) :
     sys.path.insert(0, directory)
     importlib.import_module(name)
